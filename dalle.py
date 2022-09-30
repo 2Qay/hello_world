6eedd4c655fa633f5bbc104e88714a1df928c81b
2qay <gougle2011@yandex.ru>
2022-09-30 14:36:42 +0300

    asdfg

--- dalle.py
+++ dalle.py
@@ -115,30 +115,3 @@ if prompt != "":
             container.markdown(
                 "Error -5, please try again or [report it](mailto:pcuenca-dalle@guenever.net)."
             )
-            
-            
-
-text = st.text_input("че хочешь нарисовать")
-num_images = st.slider("сколько картинок надо??", 1, 4)
-
-ok = st.button("нарисовать!!!")
-
-if ok:
-	create_and_show_images(text, num_images)
-
-
-
-#image = Image.open('ssssss.jpg')
-#st.write("ШРЕК")
-#if st.button('Say hello', on_click):
-#    st.image(image, caption = 'SHREEEEEEEEK')
-#else:
-#    st.write('Goodbye')
-
-#agree = st.checkbox('Показать шрека?')
-#if agree:
-#    st.image(image, caption = 'SHREEEEEEEEK')
-#else:
-#    st.write('Нажми на квадратик')
-
-
