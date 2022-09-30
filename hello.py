f0e33c8883c412b56dd92508e40b26b3c4773589
2qay <gougle2011@yandex.ru>
2022-09-30 13:43:36 +0300

    sdf

--- hello.py
+++ hello.py
@@ -4,9 +4,13 @@ image = Image.open('ssssss.jpg')
 st.write("ШРЕК")
 
 
-if st.button('Say hello', on_click):
+#if st.button('Say hello', on_click):
+#    st.image(image, caption = 'SHREEEEEEEEK')
+#else:
+#    st.write('Goodbye')
+
+agree = st.checkbox('I agree')
+if agree:
     st.image(image, caption = 'SHREEEEEEEEK')
 else:
-    st.write('Goodbye')
-
-
+    st.write('НИКАКОВА ВАМ ШРЕКА')
