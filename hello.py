8099759ea3f0228247d102724aad380568cbdc9d
2qay <gougle2011@yandex.ru>
2022-09-30 13:29:23 +0300

    sdf

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import streamlit as st
-#image = Image.open('ssssss.jpg')
-#image.show()
+image = Image.open('ssssss.jpg')
 st.write("lol")
 st.write("kek")
-st.write("чебурек")
+st.write("ШРЕК")
+st.image(image, caption = 'SHREEEEEEEEK')
