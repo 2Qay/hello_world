6b3718f09b6b0d64c670007f2c7f0a82babf0dce
2qay <gougle2011@yandex.ru>
2022-09-30 13:39:21 +0300

    sdf

--- hello.py
+++ hello.py
@@ -3,7 +3,8 @@ import streamlit as st
 image = Image.open('ssssss.jpg')
 st.write("ШРЕК")
 
-if st.button('Say hello'):
+
+if st.button('Say hello', on_click):
     st.image(image, caption = 'SHREEEEEEEEK')
 else:
     st.write('Goodbye')
